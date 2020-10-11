5fe3b62e75a95503f6958eed0006b53a1f6de94a
devvy <mugalovania@gmail.com>
2020-10-11 14:58:11 -0400

    magic
    amgicmagicmagicmagicmagic

--- Engine Hacks/SkillSystem/Skills/AuraSkills/Inspire/Inspire.s
+++ Engine Hacks/SkillSystem/Skills/AuraSkills/Inspire/Inspire.s
@@ -15,6 +15,13 @@ ldrb r1, [r4, #0x13] @currhp
 cmp r1, r0
 blt Done
 
+mov r0, r4       @Move attacker data into r1.
+mov r1, #0x4c    @Move to the attacker's weapon ability
+ldr r1, [r0,r1]
+mov r2, #0x42
+tst r1, r2
+bne     Done @do nothing if magic bit set
+
 @now check for the skill
 ldr r0, AuraSkillCheck
 mov lr, r0
@@ -29,7 +36,7 @@ beq Done
 mov r0, r4
 add     r0,#0x5A    @Move to the attacker's damage.
 ldrh    r3,[r0]     @Load the attacker's damage into r3.
-add     r3,#3    @add 3 to the attacker's damage.
+add     r3,#2    @add 2 to the attacker's damage.
 strh    r3,[r0]     @Store attacker avoid.
 
 Done:
