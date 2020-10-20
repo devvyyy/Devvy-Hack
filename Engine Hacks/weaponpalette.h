2a4f05354b06f77f24eb719864513ad085b6b949
devvy <mugalovania@gmail.com>
2020-10-20 16:33:30 -0400

    affiniy
    y

---
+++ Engine Hacks/weaponpalette.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(weaponpalette)
+
+//======================================================================
+//
+//	weaponpalette, 768x400@8, 
+//	+ palette 16 entries, not compressed
+//	+ 4800 tiles not compressed
+//	Total size: 32 + 307200 = 307232
+//
+//	Time-stamp: 2020-10-20, 11:10:15
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_WEAPONPALETTE_H
+#define GRIT_WEAPONPALETTE_H
+
+#define weaponpaletteTilesLen 307200
+extern const unsigned short weaponpaletteTiles[153600];
+
+#define weaponpalettePalLen 32
+extern const unsigned short weaponpalettePal[16];
+
+#endif // GRIT_WEAPONPALETTE_H
+
+//}}BLOCK(weaponpalette)
