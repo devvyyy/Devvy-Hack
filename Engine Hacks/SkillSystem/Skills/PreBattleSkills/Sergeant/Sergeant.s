94c0b8fed70cd402a668077d605628d2d8889bc0
devvy <mugalovania@gmail.com>
2020-10-30 23:57:52 -0400

    warden fix + new mugs

--- Engine Hacks/SkillSystem/Skills/PreBattleSkills/Sergeant/Sergeant.s
+++ Engine Hacks/SkillSystem/Skills/PreBattleSkills/Sergeant/Sergeant.s
@@ -1,49 +1,36 @@
 .thumb
-.align
+.equ SergeantID, SkillTester+4
 
-.equ SergeantID,SkillTester+4
+push {r4-r7, lr}
+mov r4, r0 @atkr
+mov r5, r1 @dfdr
 
-push {r4-r7,lr}
-@goes in the battle loop.
-@r0 is the attacker
-@r1 is the defender
-mov r4, r0
-mov r5, r1
+@hp not at full
+ldrb r0, [r4, #0x12] @max hp
+ldrb r1, [r4, #0x13] @curr hp
+cmp r0, r1
+bne End @skip if not max hp
 
-@check for skill
+@has Sergeant
 ldr r0, SkillTester
 mov lr, r0
-mov r0, r4
+mov r0, r4 @attacker data
 ldr r1, SergeantID
 .short 0xf800
 cmp r0, #0
-beq GoBack
-
-@make sure we're in combat (or combat prep)
-ldrb r3, =gBattleData
-ldrb r3, [r3]
-cmp r3, #4
 beq End
 
-mov r2, #0x14 @attacker str
-ldrb r0, [r4, r2] 
-ldrb r1, [r5, #0x15] @defender def
-cmp r0, r1
-ble End @skip if str is less or equal
+@apply def +7
+mov r0, r4
+add r0,#0x5C
+ldrh r3,[r0]
+add r3,#7
+strh r3,[r0]
 
-mov r1, #0x5c
-ldrh r0, [r4, r1]
-add r0, #2
-strh r0, [r4,r1]
-
-GoBack:
-pop {r4-r7}
-pop {r0}
-bx r0
-
-.ltorg
+End:
+pop {r4-r7, r15}
 .align
-
+.ltorg
 SkillTester:
-@POIN SkillTester
+@Poin SkillTester
 @WORD SergeantID
