b5e0e66bb0a3665682e22cb5ad4ddd1f3a9130a4
devvyyy <mugalovania@gmail.com>
2020-12-01 23:26:24 -0500

    i fear the edge of dawn
    magic overhaul

---
+++ Engine Hacks/SkillSystem/Skills/AuraSkills/Anima2/Anima2.s
@@ -0,0 +1,36 @@
+@Spur Def: adjacent allies gain +4 defense in combat.
+.equ Anima2ID, AuraSkillCheck+4
+.thumb
+push {r4-r7,lr}
+@goes in the battle loop.
+@r0 is the attacker
+@r1 is the defender
+mov r4, r0
+mov r5, r1
+
+@now check for the skill
+ldr r0, AuraSkillCheck
+mov lr, r0
+mov r0, r4 @attacker
+ldr r1, Anima2ID
+mov r2, #0 @can_trade
+mov r3, #1 @range
+.short 0xf800
+cmp r0, #0
+beq Done
+
+mov r0, r4
+add     r0,#0x5A    @Move to the attacker's damage.
+ldrh    r3,[r0]     @Load the attacker's damage into r3.
+add     r3,#2    @add 1 damage.
+strh    r3,[r0]     @Store dmg.
+
+Done:
+pop {r4-r7}
+pop {r0}
+bx r0
+.align
+.ltorg
+AuraSkillCheck:
+@ POIN AuraSkillCheck
+@ WORD Anima2ID
---
+++ Engine Hacks/SkillSystem/Skills/AuraSkills/Dark2/Dark2.s
@@ -0,0 +1,36 @@
+@Dark2: adjacent foes deal 2 less damage.
+.equ Dark2ID, AuraSkillCheck+4
+.thumb
+push {r4-r7,lr}
+@goes in the battle loop.
+@r0 is the attacker
+@r1 is the defender
+mov r4, r0
+mov r5, r1
+
+CheckSkill:
+@now check for the skill
+ldr r0, AuraSkillCheck
+mov lr, r0
+mov r0, r4 @attacker
+ldr r1, Dark2ID
+mov r2, #3 @are enemies
+mov r3, #1 @range
+.short 0xf800
+cmp r0, #0
+beq Done
+
+mov r1, #0x5A
+ldrh r0, [ r4, r1 ]
+sub r0, r0, #2
+strh r0, [ r4, r1 ]
+
+Done:
+pop {r4-r7}
+pop {r0}
+bx r0
+.align
+.ltorg
+AuraSkillCheck:
+@ POIN AuraSkillCheck
+@ WORD Dark2ID
