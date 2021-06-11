abbf0ce71a756475e4fcf42dba8ef23d05e8b781
devvyyy <mugalovania@gmail.com>
2021-06-11 17:05:09 -0400

    i feel at peace
    preparations for stat spread changes

---
+++ Engine Hacks/SkillSystem/Skills/PreBattleSkills/FinalBossTome/FinalBoss.s
@@ -0,0 +1,43 @@
+.thumb
+.equ FinalBossID, SkillTester+4
+
+push {r4-r7, lr}
+mov r4, r0 @atkr
+mov r5, r1 @dfdr
+
+@has FinalBoss
+ldr r0, SkillTester
+mov lr, r0
+mov r0, r4 @attacker data
+ldr r1, FinalBossID
+.short 0xf800
+cmp r0, #0
+beq End
+
+@make sure we're in combat (or combat prep)
+ldrb r3, =gBattleData
+ldrb r3, [r3]
+cmp r3, #4
+beq End
+
+mov r2, #0x3A @attacker mag
+ldrb r0, [r4, r2] 
+ldrb r1, [r5, #0x14] @defender str
+cmp r0, r1
+ble End @skip if mag is less or equal
+
+@add enemy's max hp/4 damage
+mov  r1, #0x5A
+ldrh r0, [r4, r1] @strength
+ldrb r2, [r5, #0x12] @max hp
+lsr  r2, #2
+add  r0, r2
+strh r0, [r4,r1]
+
+End:
+pop {r4-r7, r15}
+.align
+.ltorg
+SkillTester:
+@Poin SkillTester
+@WORD FinalBossID
